48cb67d9b3fae197f92d3a546b687d37a9b45229
Joachim Løvgaard <joachim@loevgaard.dk>
2023-08-09 15:37:06 +0200

    Try to fix ECS

--- ecs.php
+++ ecs.php
@@ -2,16 +2,16 @@
 
 declare(strict_types=1);
 
-use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\ValueObject\Option;
 
-return static function (ECSConfig $config): void {
+return static function (ContainerConfigurator $config): void {
     $config->import('vendor/sylius-labs/coding-standard/ecs.php');
-    $config->paths([
+    $config->parameters()->set(Option::PATHS, [
         'src',
-        'tests',
+        'tests'
     ]);
-    $config->skip([
-        'tests/Application/node_modules/**',
-        'tests/Application/var/**',
+    $config->parameters()->set(Option::SKIP, [
+        'tests/Application/**'
     ]);
 };

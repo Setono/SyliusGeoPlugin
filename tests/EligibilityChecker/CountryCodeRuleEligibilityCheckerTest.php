8891a10fbde17b81c4e65ab4357a27c2f0d07842
Joachim Løvgaard <joachim@loevgaard.dk>
2023-08-14 12:10:16 +0200

    Add assertion

--- tests/EligibilityChecker/CountryCodeRuleEligibilityCheckerTest.php
+++ tests/EligibilityChecker/CountryCodeRuleEligibilityCheckerTest.php
@@ -46,4 +46,19 @@ final class CountryCodeRuleEligibilityCheckerTest extends TestCase
         $checker = new CountryCodeRuleEligibilityChecker($countryCodeProvider->reveal());
         self::assertFalse($checker->isEligible($rule));
     }
+
+    /**
+     * @test
+     */
+    public function it_is_not_eligible_if_the_country_code_is_null(): void
+    {
+        $rule = new Rule();
+        $rule->setCountryCodes(['DK']);
+
+        $countryCodeProvider = $this->prophesize(CountryCodeProviderInterface::class);
+        $countryCodeProvider->getCountryCode()->willReturn(null);
+
+        $checker = new CountryCodeRuleEligibilityChecker($countryCodeProvider->reveal());
+        self::assertFalse($checker->isEligible($rule));
+    }
 }
